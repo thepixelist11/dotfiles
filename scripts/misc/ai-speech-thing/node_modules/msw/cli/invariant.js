496f036a34f882cffb4197919a90ffb34fade9ea
thepixelist11 <benaneeb@gmail.com>
2025-03-16 19:38:48 -0400

    03-16-2025

--- scripts/misc/ai-speech-thing/node_modules/msw/cli/invariant.js
+++
@@ -1,8 +0,0 @@
-const colors = require('picocolors')
-
-module.exports = function invariant(predicate, message, ...args) {
-  if (!predicate) {
-    console.error(colors.red(message), ...args)
-    process.exit(1)
-  }
-}
